61f70d6ceca2e8511d5061bf58b4213cc96b4c5d
Jussivan <jussivanbm1029@gmail.com>
2025-03-04 13:20:01 -0300

    install PG and configure env/index.ts

--- aula2/src/database.ts
+++ aula2/src/database.ts
@@ -2,15 +2,12 @@ import { knex as setupKnex, Knex } from "knex";
 import { env } from "./env";
 
 export const config: Knex.Config = {
-    client: 'sqlite',
-    connection: {
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_CLIENT === 'sqlite' ? {
         filename: env.DATABASE_URL,
-    },
+    } : env.DATABASE_URL,
     migrations: {
         directory: './db/migrations',
-    },
-    seeds: {
-        directory: './seeds',
     },
     useNullAsDefault: true,
 }
